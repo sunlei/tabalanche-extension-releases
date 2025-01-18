5070fc4c539ba51237cbdd02a46f9273fa93dff0
sunlei <guizaicn@gmail.com>
2025-01-19 00:20:01 +0800

    feat: add workflow

--- scripts/update-updates.sh
+++ scripts/update-updates.sh
@@ -5,11 +5,13 @@ set -o errexit  # set -e
 set -o errtrace # set -E
 set -o pipefail
 
+XPI_URL="https://github.com/sunlei/tabalanche-extension-releases/releases/download/v$CALVER_VERSION/tabalanche-$CALVER_VERSION.xpi"
+
 jq --raw-output \
     --arg GECKO_ID "${GECKO_ID}" \
     --arg CALVER_VERSION "${CALVER_VERSION}" \
-    --arg XPIPATH "${XPIPATH}" \
-    '.addons[$GECKO_ID].updates += [{"version": $CALVER_VERSION, "update_link": $XPIPATH }]' \
+    --arg XPI_URL "${XPI_URL}" \
+    '.addons[$GECKO_ID].updates += [{"version": $CALVER_VERSION, "update_link": $XPI_URL }]' \
     updates.json >/tmp/updates.json
 mv /tmp/updates.json updates.json
 

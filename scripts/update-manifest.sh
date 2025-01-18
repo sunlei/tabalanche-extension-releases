2e5152e6ff398c78c819a4711333f68d97ceef4f
sunlei <guizaicn@gmail.com>
2025-01-18 23:11:16 +0800

    feat: add workflow

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -11,21 +11,21 @@ mv /tmp/manifest.json src/manifest.json
 
 # Update `version`
 jq \
-    --arg version "{$CALVER_VERSION}" \
+    --arg version "$CALVER_VERSION" \
     '.version = $version' \
     src/manifest.json >/tmp/manifest.json
 mv /tmp/manifest.json src/manifest.json
 
 # Update `gecko.id`
 jq \
-    --arg gecko_id "{$GECKO_ID}" \
+    --arg gecko_id "$GECKO_ID" \
     '.browser_specific_settings.gecko.id = $gecko_id' \
     src/manifest.json >/tmp/manifest.json
 mv /tmp/manifest.json src/manifest.json
 
 # Update `gecko.update_url`
 jq \
-    --arg update_url "{$UPDATE_URL}" \
+    --arg update_url "$UPDATE_URL" \
     '.browser_specific_settings.gecko.update_url = $update_url' \
     src/manifest.json >/tmp/manifest.json
 mv /tmp/manifest.json src/manifest.json

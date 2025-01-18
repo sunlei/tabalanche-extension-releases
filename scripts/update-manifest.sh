0e76296ed4bb1107f291c163825d92040106035e
sunlei <guizaicn@gmail.com>
2025-01-18 22:58:32 +0800

    feat: add workflow

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -6,26 +6,30 @@ set -o errtrace # set -E
 set -o pipefail
 
 
-# Update manifest.json with version
+# Delete `key`
+jq 'del(.key)' src/manifest.json > /tmp/manifest.json
+mv /tmp/manifest.json src/manifest.json
+
+# Update `version`
 jq \
     --arg version "$CALVER_VERSION" \
     '.version = $version' \
-    src/manifest.json > /tmp/manifest.tmp.json
-    mv /tmp/manifest.tmp.json src/manifest.json
+    src/manifest.json > /tmp/manifest.json
+mv /tmp/manifest.json src/manifest.json
 
-# Update manifest.json with gecko.id
+# Update `gecko.id`
 jq \
     --arg gecko_id "$GECKO_ID" \
     '.browser_specific_settings.gecko.id = $gecko_id' \
-    src/manifest.json > /tmp/manifest.tmp.json
-    mv /tmp/manifest.tmp.json src/manifest.json
+    src/manifest.json > /tmp/manifest.json
+mv /tmp/manifest.json src/manifest.json
 
-# Update manifest.json with gecko.update_url
+# Update `gecko.update_url`
 jq \
     --arg update_url "$UPDATE_URL" \
     '.browser_specific_settings.gecko.update_url = $update_url' \
-    src/manifest.json > /tmp/manifest.tmp.json
-    mv /tmp/manifest.tmp.json src/manifest.json
+    src/manifest.json > /tmp/manifest.json
+mv /tmp/manifest.json src/manifest.json
 
 
 

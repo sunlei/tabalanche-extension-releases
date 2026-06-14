760f33ed3f20933ab3e9ad56c4547e1ea4c1bcf2
sunlei <guizaicn@gmail.com>
2026-06-14 23:36:47 +0800

    ci: fix extension source dir
    - Set the checked-out extension source directory explicitly
    - Update manifest mutation to use the configured source path

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -5,29 +5,31 @@ set -o errexit  # set -e
 set -o errtrace # set -E
 set -o pipefail
 
+MANIFEST_PATH="${EXTENSION_SOURCE_DIR:-src}/manifest.json"
+
 # Delete `key`
-jq 'del(.key)' src/manifest.json >/tmp/manifest.json
-mv /tmp/manifest.json src/manifest.json
+jq 'del(.key)' "$MANIFEST_PATH" >/tmp/manifest.json
+mv /tmp/manifest.json "$MANIFEST_PATH"
 
 # Update `version`
 jq \
     --arg version "$CALVER_VERSION" \
     '.version = $version' \
-    src/manifest.json >/tmp/manifest.json
-mv /tmp/manifest.json src/manifest.json
+    "$MANIFEST_PATH" >/tmp/manifest.json
+mv /tmp/manifest.json "$MANIFEST_PATH"
 
 # Update `gecko.id`
 jq \
     --arg gecko_id "$GECKO_ID" \
     '.browser_specific_settings.gecko.id = $gecko_id' \
-    src/manifest.json >/tmp/manifest.json
-mv /tmp/manifest.json src/manifest.json
+    "$MANIFEST_PATH" >/tmp/manifest.json
+mv /tmp/manifest.json "$MANIFEST_PATH"
 
 # Update `gecko.update_url`
 jq \
     --arg update_url "$UPDATE_URL" \
     '.browser_specific_settings.gecko.update_url = $update_url' \
-    src/manifest.json >/tmp/manifest.json
-mv /tmp/manifest.json src/manifest.json
+    "$MANIFEST_PATH" >/tmp/manifest.json
+mv /tmp/manifest.json "$MANIFEST_PATH"
 
-cat src/manifest.json
+cat "$MANIFEST_PATH"
